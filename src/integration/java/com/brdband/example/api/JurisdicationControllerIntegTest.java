503d307026634e2525bd9ba1cb928f93199dd15e
Rajkumar <Rajkumar.Jayakumar@cognizant.com>
2018-03-20 14:41:39 +0530

    Added Status OK Test

--- src/integration/java/com/brdband/example/api/JurisdicationControllerIntegTest.java
+++ src/integration/java/com/brdband/example/api/JurisdicationControllerIntegTest.java
@@ -1,19 +1,19 @@
 package com.brdband.example.api;
 
 import com.brdband.example.api.models.CircleJurisdication;
-import com.brdband.example.api.repositories.JurisdicationRepository;
+import com.brdband.example.api.models.ResponseMessage;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
 import static org.springframework.test.util.AssertionErrors.assertTrue;
 
 @RunWith(SpringRunner.class)
@@ -37,13 +37,18 @@ public class JurisdicationControllerIntegTest {
     HttpHeaders headers = new HttpHeaders();
 
     @Test
-    public void testDemo(){
-              assertTrue("*******************************Running Integration Test", true);
+    public void testDemo() {
+        assertTrue("*******************************Running Integration Test", true);
     }
 
+    @Test
+    public void testForResonseCodeOK() {
 
+        ResponseEntity<ResponseMessage> response = restTemplate.
+                getForEntity("http://localhost:9002/getCircleById?circleId=1", ResponseMessage.class);
 
-
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+    }
 
 
 }
